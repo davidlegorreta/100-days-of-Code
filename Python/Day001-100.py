bfb7fcb8e9f4b8aba2482c13f79a26e64bbffaa3
davidlegorreta <davidlegorreta@comunidad.unam.mx>
2021-10-04 18:10:05 -0500

    Create Day001-100.py
    #100 days of code. Day 001. Concatenation of strings #

---
+++ Python/Day001-100.py
@@ -0,0 +1,20 @@
+#100 days of code. Day 001. Concatenation of strings #
+
+#1. Create a greeting for your program.
+
+print ("Hello! This first program can be halpful for your future band!")
+
+#2. Ask the user for the city that they grew up in.
+
+city = input ("Please, insert your city: \n")
+
+#3. Ask the user for the name of a pet.
+
+pet = input ("Please, tell me the name of your pet: \n")
+
+#4. Combine the name of their city and pet and show them their band name.
+
+print ("Cnograts! Your Band name is: " + city + " " + pet)
+
+#5. Make sure the input cursor shows on a new line, see the example at:
+#   https://band-name-generator-end.appbrewery.repl.run/
